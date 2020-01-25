2063fcf2f9ffbbfb1592780151274cc0dc6dd02e
Priya <pvaidya56@gmail.com>
2020-01-24 19:21:43 -0800

    added data to the state

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
 
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ data: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    return <div className="App"></div>
+  }
+}
 export default App;
